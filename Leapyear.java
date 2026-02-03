2996af9526bfa036a81ce2221d338921c10e74ca
Anulakshmi T <anushanthi2005@gmail.com>
2026-02-03 10:24:12 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,15 @@
+ package JavaBasic;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		   int year= 2024;
+		   if((year % 400 == 0)||(year % 4 == 0 && year % 100 != 0)){
+			   System.out.println("Leap Year");
+		   }else {
+			   System.out.println("not a Leap Year");
+		   }
+
+	}
+
+}
